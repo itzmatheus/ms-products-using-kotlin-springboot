c0e849e5aff1acb39f7a1f7427a02c95025c06f4
matheus.leite <matheusjoselfm@gmail.com>
2023-04-13 22:48:39 -0300

    test(ProductController): should update product

--- src/test/kotlin/com/kotlin/springboot/kotlin/controller/ProductControllerTest.kt
+++ src/test/kotlin/com/kotlin/springboot/kotlin/controller/ProductControllerTest.kt
@@ -3,6 +3,7 @@ package com.kotlin.springboot.kotlin.controller
 import com.kotlin.springboot.kotlin.entity.Category
 import com.kotlin.springboot.kotlin.entity.Product
 import com.kotlin.springboot.kotlin.repository.CategoryRepository
+import com.kotlin.springboot.kotlin.repository.ProductRepository
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -18,6 +19,9 @@ class ProductControllerTest {
     @Autowired
     lateinit var categoryRepository: CategoryRepository
 
+    @Autowired
+    lateinit var productRepository: ProductRepository
+
     @Test
     @Order(1)
     fun shouldAddProducts() {
@@ -31,6 +35,26 @@ class ProductControllerTest {
         Assertions.assertNotNull(productReturned)
         Assertions.assertNotNull(productReturned.id)
         Assertions.assertEquals(product.name, productReturned.name)
+        Assertions.assertEquals(product.category?.name, productReturned.category?.name)
+
+    }
+
+    @Test
+    @Order(2)
+    fun shouldUpdateProduct() {
+        var category = Category(name = "ELETRONIC")
+        category = categoryRepository.save(category)
+        var product = Product(name = "iPhone 11", price = 2500, category = category)
+        product = productRepository.save(product)
+
+        product.name = "iPhone 12"
+        template.put("/products", product)
+
+        val productReturned = template.getForObject("/products/{id}", Product::class.java, product.id)
+
+        Assertions.assertNotNull(productReturned)
+        Assertions.assertNotNull(productReturned.id)
+        Assertions.assertEquals("iPhone 12", productReturned.name)
         Assertions.assertEquals(product.category?.name, productReturned.category?.name)
 
     }
