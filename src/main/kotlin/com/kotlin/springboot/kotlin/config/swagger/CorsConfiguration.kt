7babe35c0104b1939229af4a5c5b8e56f9a5e12b
Matheus Leite <matheusjoselfm@gmail.com>
2024-06-06 19:42:47 -0300

    removido skaffold

---
+++ src/main/kotlin/com/kotlin/springboot/kotlin/config/swagger/CorsConfiguration.kt
@@ -0,0 +1,23 @@
+package com.kotlin.springboot.kotlin.config.swagger
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfiguration {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOriginPatterns("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowCredentials(true)
+            }
+        }
+    }
+
+}
